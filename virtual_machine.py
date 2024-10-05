a64d4efa497d018967e95a72a77dbcd8044cb489
chrisbodhi <chris.boette@data.world>
2024-10-05 15:08:17 -0400

    Add memory stubs.

--- virtual_machine.py
+++ virtual_machine.py
@@ -1,24 +1,32 @@
 from abc import ABC, abstractmethod
-from typing import Callable, List, Union
+from typing import Any, Callable, List, Union
 import struct
 
 WasmValue = Union[int, float]
 
 def i32(value: int) -> int:
-    # Truncate numbers greater than 32 bits down to 32 bits
+    """
+    Truncate numbers greater than 32 bits down to 32 bits
+    """
     return value & 0xFFFFFFFF
 
 def i64(value: int) -> int:
-    # Truncate numbers greater than 64 bits down to 64 bits
+    """
+    Truncate numbers greater than 64 bits down to 64 bits
+    """
     return value & 0xFFFFFFFFFFFFFFFF
 
 def f32(value: float) -> float:
-    # Ensures that the float is a 32-bit float
-    # N.B. Python's float type is 64-bit
+    """
+    Ensures that the float is a 32-bit float
+    N.B. Python's float type is 64-bit
+    """
     return struct.unpack('f', struct.pack('f', value))[0]
 
 def f64(value: float) -> float:
-    # Python's float is already 64-bit
+    """
+    Identity function; Python's float is already 64-bit
+    """
     return value
 
 num_fns = {
@@ -31,15 +39,23 @@ num_fns = {
 supported_value_types = list(num_fns.keys())
 
 class VMState:
-    def __init__(self):
+    def __init__(self, pages: int, max_pages: int):
         self.stack: List[WasmValue] = []
         self.pc: int = 0
+        # TODO: Memory of 10KiB per page
+        # TODO: make this a byte array with 10KiB of
+        # blank bytes per "page"
+        # TODO: replace the None
+        self.memory = [None] * pages
+        self.max_pages = max_pages
+
 
 class Instruction(ABC):
     @abstractmethod
-    def execute(self, state: VMState) -> None:
+    def execute(self, state: VMState) -> Any:
         pass
 
+### Stack operations -- begin ###
 class Push(Instruction):
     """Push is an instruction that pushes a value onto the top of the stack."""
     def __init__(self, value: WasmValue):
@@ -52,6 +68,19 @@ class Pop(Instruction):
     """Pop is an instruction that removes the first value from the stack and returns it."""
     def execute(self, state: VMState) -> WasmValue:
         return state.stack.pop()
+
+class Grow(Instruction):
+    """
+    Grow is an instruction to increase the size of memory by a page, up until
+    reaching the maximum specified when initializing the VM.
+    """
+    def execute(self, state: VMState) -> None:
+        # TODO: define a `makePage` function
+        page = None
+        if len(state.memory) < state.max_pages:
+            state.memory.append(page)
+
+### Stack operations -- end ###
 
 class Add(Instruction):
     """Add is an instruction that pops the top two values from the stack, adds them together, and pushes the result back onto the stack."""
@@ -80,8 +109,8 @@ class Sub(Instruction):
         Push(self.fn(a - b)).execute(state)
 
 class StackVM:
-    def __init__(self):
-        self.state = VMState()
+    def __init__(self, pages: int = 0, max_pages: int = 0):
+        self.state = VMState(pages, max_pages)
         self.instructions: List[Instruction] = []
         self.observers: List[Callable[[VMState], None]] = []
 
@@ -91,8 +120,8 @@ class StackVM:
     def notify_observers(self) -> None:
         for observer in self.observers:
             observer(self.state)
-    
-    def inspect(self) -> VMState:
+
+    def inspect(self) -> List:
         return self.state.stack
 
     def execute(self, instruction: Instruction) -> None:
